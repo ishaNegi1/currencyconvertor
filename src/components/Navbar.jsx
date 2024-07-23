634208986563c823963af62b9a78e2fdc5dbbec0
ishaNegi1 <negi.isha98@gmail.com>
2024-07-23 13:31:26 +0530

    change logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-start px-8 py-2 border-slate-400 border-b-2'>
+    <div className='flex items-center justify-start px-8 py-4 border-slate-400 border-b-2'>
     <div>
-       <img src="logo.png" alt="logo" className=' w-20 h-20'/>
+       <img src="logo.png" alt="logo" className=' sm:w-12 sm:h-12 w-10 h-10'/>
        </div>
        <div>
       <h1
-        className=" text-white sm:text-4xl text-xl font-semibold"
+        className=" text-white sm:text-4xl text-xl font-semibold ml-2"
       >
         Currency Convertor
       </h1>
